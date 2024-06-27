ceba77ecbb6948437b64e5f3ce29df05cff06b91
Abid191 <abid15-12563@diu.edu.bd>
2024-06-27 17:12:02 +0600

    3rd

--- DayOne/DayOne.py
+++ DayOne/DayOne.py
@@ -1,8 +1,44 @@
-name = input("Inter your organization name: ")
-print('organization name is: '+ name) 
+#name = input("Inter your organization name: ")
+#print('organization name is: '+ name) 
 
 # + (something its call concatenate) 
 #camelCaseWord 
 #PascalCaseWord 
 #snake_case_word 
-#secOne
+#secOne 
+
+#multiple Assignment variable 
+
+a=b=c= 35 
+print(a)
+print(b)
+print(c)
+
+a,b,c= 35,36,37 
+print(a)
+print(b)
+print(c)
+
+a = 'Abid' 
+print(type(a))  
+
+
+# a = input("inter 1st number: ")
+# b = input("inter 2nd number: ")
+
+# sum = int(a) + int(b) 
+# print(sum) 
+
+#slice
+
+a = " welcome to python" 
+print(a[8:11]) 
+
+# Float
+
+a = float(input( "inter your 1st number: "))
+b = float(input( "inter your 2nd number: ")) 
+
+sum= a + b 
+print(sum)
+print(type(sum))
